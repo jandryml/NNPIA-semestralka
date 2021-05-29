f7775d8c65d325bffaf123543598d52b8860138b
Jan Dryml <jan.dryml@mainware.com>
2021-05-29 04:10:54 +0200

    init

---
+++ src/test/kotlin/cz/edu/upce/ServerApplicationTest.kt
@@ -0,0 +1,13 @@
+package cz.edu.upce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServerApplicationTest {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
